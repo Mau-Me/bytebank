6dd9dcff1621751b350fb3988352a3ca0368f03f
Mau-Me <mauriciosm95@gmail.com>
2021-12-24 17:56:28 -0300

    Atualização do arquivo main

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: Scaffold(
-          body: ListaTranferencias(),
-          appBar: AppBar(
-            title: const Text('Transferências'),
-            backgroundColor: Colors.blueGrey[900],
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            child: const Icon(Icons.add),
-            backgroundColor: Colors.blueGrey[900],
+import 'screens/transferencia/lista.dart';
+
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ListaTransferencias(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.blueGrey,
+          accentColor: Colors.black,
+        ).copyWith(
+          secondary: Colors.lightBlueAccent[700],
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.lightBlueAccent[700],
           ),
         ),
       ),
     );
-
-class ListaTranferencias extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      ItemTransferencia(Transferencia(100.0, 572)),
-      ItemTransferencia(Transferencia(100.0, 572)),
-      ItemTransferencia(Transferencia(100.0, 572)),
-      ItemTransferencia(Transferencia(147.0, 572)),
-      ItemTransferencia(Transferencia(200.0, 572)),
-    ]);
   }
-}
-
-class ItemTransferencia extends StatelessWidget {
-  final Transferencia _transferencia;
-  ItemTransferencia(this._transferencia);
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        leading: const Icon(Icons.monetization_on),
-        title: Text(_transferencia.valor.toString()),
-        subtitle: Text(_transferencia.numeroConta.toString()),
-      ),
-    );
-  }
-}
-
-class Transferencia {
-  final double valor;
-  final int numeroConta;
-
-  Transferencia(this.valor, this.numeroConta);
 }
