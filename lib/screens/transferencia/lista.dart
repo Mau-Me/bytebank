ff4df1952d690e6ce198486d85f5a1e9d336c764
Mauricio Menezes <mauriciosm95@gmail.com>
2022-01-11 11:26:26 -0300

    Adiciona botões de editar e apagar

--- lib/screens/transferencia/lista.dart
+++ lib/screens/transferencia/lista.dart
@@ -4,15 +4,6 @@ import 'package:bytebank/screens/transferencia/formulario.dart';
 import 'package:bytebank/constant/constants.dart';
 import 'package:flutter/material.dart';
 
-/*body: ListView.builder(
-          itemCount: widget._transferencias.length,
-          itemBuilder: (context, indice) {
-            final transferencia = widget._transferencias[indice];
-            return ItemTransferencia(transferencia);
-          },
-        )
-*/
-
 class ListaTransferencias extends StatefulWidget {
   const ListaTransferencias({Key? key}) : super(key: key);
 
@@ -88,11 +79,25 @@ class ItemTransferencia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: ListTile(
-        leading: const Icon(Icons.monetization_on),
-        title: Text(_transferencia.valor.toString()),
-        subtitle: Text(_transferencia.numeroConta.toString()),
+        child: ListTile(
+      leading: const Icon(Icons.monetization_on),
+      title: Text(_transferencia.valor.toString()),
+      subtitle: Text(_transferencia.numeroConta.toString()),
+      trailing: SizedBox(
+        width: 100,
+        child: Row(children: [
+          IconButton(
+            icon: const Icon(Icons.edit),
+            color: Colors.orange[300],
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.delete),
+            color: Colors.red[300],
+            onPressed: () {},
+          ),
+        ]),
       ),
-    );
+    ));
   }
 }
